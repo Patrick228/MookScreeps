a2811729ceae6917945a703769bf7975d0044fc0
Patrick H <iluvcheez227@gmail.com>
2017-11-09 21:41:40 -0700

    Removed Emoticons From Status Messages
    Not supported by Notepad++, will be solved at a later date but for
    consistency I have removed the messages.

--- Modules/role.builder.js
+++ Modules/role.builder.js
@@ -5,11 +5,11 @@ var roleBuilder = {
 
 	    if(creep.memory.building && creep.carry.energy == 0) {
             creep.memory.building = false;
-            creep.say('?? harvest');
+            creep.say('harvest');
 	    }
 	    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.building = true;
-	        creep.say('?? build');
+	        creep.say('build');
 	    }
 
 	    if(creep.memory.building) {
