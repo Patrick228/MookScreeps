bb6acbb5f442b55d79637de96fd5bb3c63fd87c3
Patrick H <iluvcheez227@gmail.com>
2017-11-11 16:47:25 -0700

    Updated Caps
    also removed an unknown character from spawning process

--- Modules/main.js
+++ Modules/main.js
@@ -3,31 +3,26 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
-    var spawnTaken = 0
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     console.log('Upgraders: ' + upgraders.length);
-    if(upgraders.length < 4) {
+    if(upgraders.length < 5) {
         var newNameU = 'Upgrader' + Game.time;
         console.log('Spawning new upgrader: ' + newNameU);
         Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newNameU,
             {memory: {role: 'upgrader'}});
-            var spawnTaken = 1
     }
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
     console.log('Builders: ' + builders.length);
-    if(builders.length < 5) {
+    if(builders.length < 3) {
         var newNameB = 'Builder' + Game.time;
-        if(spawnTaken = 0) {
-            console.log('Spawning new builder: ' + newNameB);
+        console.log('Spawning new builder: ' + newNameB);
         Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], newNameB,
             {memory: {role: 'builder'}});
-        }
-        
     }
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
-    if(harvesters.length < 8) {
+    if(harvesters.length < 6) {
         var newNameH = 'Harvester' + Game.time;
         console.log('Spawning new harvester: ' + newNameH);
         Game.spawns['Spawn1'].spawnCreep([WORK, CARRY,  MOVE], newNameH,
@@ -36,7 +31,7 @@ module.exports.loop = function () {
         if(Game.spawns['Spawn1'].spawning) { 
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
-            '???' + spawningCreep.memory.role,
+            'Spawn' + spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1, 
             Game.spawns['Spawn1'].pos.y, 
             {align: 'left', opacity: 0.8});
